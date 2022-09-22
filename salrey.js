b2bb0121a454d356c9c239d91efb3f3b011e50a8
Shivam <114128485+Shivamchy@users.noreply.github.com>
2022-09-22 11:15:29 +0530

    Add files via upload

---
+++ salrey.js
@@ -0,0 +1,10 @@
+<script>
+
+function sum()
+let a=document.getElementById("t1").value;   
+let b=document.getElementById("t2").value;
+let c=parseInt(a)+parseInt(b);
+document.getElementById("t3").innerHTML=c;  
+
+
+</script>
